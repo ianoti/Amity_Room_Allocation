af7775a081b583e15405e1268307fb53506e9e7b
ianoti <ianodhiambootieno@gmail.com>
2016-12-06 15:13:45 +0300

    added occupant attribute to room model

--- models/room.py
+++ models/room.py
@@ -1,9 +1,10 @@
 #! usr/bin/env
 class Room(object):
-    def __init__(self, name, capacity=0, access_allowed=[]):
+    def __init__(self, name, capacity=0, access_allowed=[], occupants=[]):
         self.name = name
         self.capacity = capacity
         self.access_allowed = access_allowed
+        self.occupants = occupants
 
 class Office(Room):
     def __init__(self, name):
