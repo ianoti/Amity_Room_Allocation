7a0bea9ca8cc57de7544a715c2fb272e5988ee2b
ianoti <ianodhiambootieno@gmail.com>
2016-12-07 09:33:09 +0300

    added test for room creation method in Amity

--- test/test_app.py
+++ test/test_app.py
@@ -1,9 +1,22 @@
 #! usr/bin/env
 import unittest
 
-from app import Amity
+from app import *
 
-class TestSessionInitialisation(unittest.TestCase):
-    def test_session_start(self):
-        session = Amity()
-        self.assertIsInstance(session, Amity, msg = "failed to initialise Amity")
+class TestCreation(unittest.TestCase):
+    """ Initialise the Amity class """
+
+    def setUp(self):
+        self.amity = Amity()
+
+    """ check that Amity can add rooms """
+    def test_room_creation(self):
+        self.amity.add_room("Hogwarts", "o")
+        self.amity.add_room("Narnia", "l")
+        self.assertNotEqual(0, len(self.amity.room_directory), msg='rooms did not successfully add to room_directory')
+        self.assertEqual(2, len(self.amity.room_directory), msg='incorrect number of room added')
+        self.assertIsInstance(self.amity.room_directory[0], Office, msg="incorrect instantiation")
+        self.assertIsInstance(self.amity.room_directory[1], LivingSpace, msg="incorrect instantiation")
+        self.assertListEqual([self.amity.room_directory[0].name,
+            self.amity.room_directory[1].name], ["Hogwarts", "Narnia"],
+            msg="room objects not properly added to room_directory")
