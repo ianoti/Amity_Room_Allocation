136ada94bd95c4e74ad7300be3e18d8283066a74
ianoti <ianodhiambootieno@gmail.com>
2016-12-21 01:05:04 +0300

    (feature) added alllocation functionality to Amity

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ class Amity(object):
     def __init__(self):
         self.room_directory = []
         self.waiting_list = []
+        self.living_waiting_list = []
 
     def add_person(self, fname, sname, role="none", wants_living = "N"):
         """ method to add people to system utilising models"""
@@ -43,7 +44,8 @@ class Amity(object):
                         room = Office(rm_variable)
                     else:
                         print ("the option given to create room is invalid")
-                    self.room_directory.append(room)
+                    new_room = [room]
+                    self.room_directory.extend(new_room)
                 else:
                     return "the room already exists"
             elif not isinstance(rm_type, str):
@@ -51,17 +53,55 @@ class Amity(object):
             elif not isinstance(rm_name, str):
                 return "the room name is invalid"
 
-    def available_rooms(self):
-        for room in
+    def get_available_space(self, room_type):
+        """ filter the room directory to return available rooms for occupancy """
+        if room_type == "o":
+            office_w_space = [room for room in self.room_directory
+                if (len(room.occupants)< room.capacity) and isinstance(room, Office)]
+            return office_w_space
+        elif room_type == "l":
+            living_w_space = [room for room in self.room_directory
+                if (len(room.occupants)< room.capacity) and isinstance(room, LivingSpace)]
+            return living_w_space
 
     def allocate(self):
-        for person in self.waiting_list:
-            
+        """ assign people to rooms """
+        available_office = self.get_available_space("o")
+        available_living = self.get_available_space("l")
+        #make a copy of waiting list to allow remove() method to run bug free
+        iteration_list = [guys for guys in self.waiting_list]
+        if len(available_office) == 0 and len(available_living) == 0:
+            print("Can't allocate no Rooms available")
+        elif len(available_office) >= 1:
+            for person in iteration_list:
+                if person.role == "staff":
+                    allocated_office = random.choice(available_office)
+                    allocated_office.occupants.append(person)
+                    for guy in allocated_office.occupants:
+                        print (guy.fname)
+                    self.waiting_list.remove(person)
+                elif person.role == "fellow" and person.wants_living.lower() == "n":
+                    allocated_office = random.choice(available_office)
+                    allocated_office.occupants.append(person)
+                    self.waiting_list.remove(person)
+                elif person.role == "fellow" and person.wants_living.lower() == "y":
+                    if len(available_living) >= 1:
+                        allocated_office = random.choice(available_office)
+                        allocated_living = random.choice(available_living)
+                        allocated_living.occupants.append(person)
+                        allocated_office.occupants.append(person)
+                        self.waiting_list.remove(person)
+                    else:
+                        self.living_waiting_list.append(person)
+                        return "Can't allocate Living Space"
 
     def reallocate(self, fname, sname, new_rm):
         """ method to allow for reallocation of users between rooms """
         pass
 
+    def print_unallocated(self):
+        """ this will display a list of unallocated people on screen optional writing to file"""
+        pass
     def batch_add_person(self, file_path):
         """  method to load names from txt file and add people to system """
         with open(file_path, "r") as people_file:
@@ -87,10 +127,15 @@ class Amity(object):
         """ method to load state from database """
         pass
 
-c = Amity()
-c.add_person("Judy", "Okatch", "staff")
-# c.add_room("o", "Hogwarts")
-print(c.room_directory)
-print(c.waiting_list)
 
+c = Amity()
+c.add_room("o", "Valhalla")
+c.add_room("l", "Tweepy")
+c.add_person("Tom", "Omondi", "STAFF")
+c.add_person("Bob", "Omondi", "STAFF")
+c.add_person("Jerry", "Omondi", "STAFF")
+c.add_person("RImothy", "Omondi", "STAFF")
+c.add_person("blake", "Omondi", "STAFF")
+print("initial waiting list", len(c.waiting_list))
 c.allocate()
+print("final length", len(c.waiting_list))
